2ec11bd4d4d63c378529416f18bece26b0989d4b
maheshtnt <maheshtnt007@gmail.com>
2025-11-11 21:16:15 -0500

    Redesigned the project card to include title

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -5,13 +5,13 @@ import githubIcon from '@/assets/githubIconWhite.svg';
 function ProjectCard(title: string, desc: string, image: string, githubLink = "") {
     return (
         <div className="flex h-[40rem] items-center justify-center">
-              <div className="hoverBase relative bg-foreground text-background flex items-center justify-center flex-col gap-6 rounded-xl shadow-sm size-9/10">
-                <div className="absolute h-full w-full flex flex-auto">
-                  {/* <div className="flex text-9xl text-justify items-center">
-                    <div>Culinary<br />Compass</div>
-                  </div> */}
-                    <img src={image} className="h-full"></img>
+              <div className="hoverBase relative bg-foreground text-background flex items-center justify-center flex-col rounded-xl shadow-black shadow-xl size-9/10">
+                <div className="h-8/10 w-full flex flex-auto">
+                    <img src={image} className="h-full w-full"></img>
                 </div> 
+                <div className="h-2/10 w-full flex flex-auto items-center align-middle">
+                  <h3 className="text-4xl text-center w-full">{title}</h3>
+                </div>
                 <div className="hoverTop absolute w-full h-full bg-foreground text-4xl text-justify items-center flex p-4">
                   <div className="text-justify items-center flex flex-col">
                     {desc}
